832a93c50b006d9ec73d9ca9e7248c3bc6b51348
AverageDude <joshandre071605@gmail.com>
2023-03-06 20:57:41 +0800

    edited index.html, index.js, index.css

---
+++ assets/js_files/index.js
@@ -0,0 +1,10 @@
+var background = document.getElementById("background");
+var colors = ['#FF5733', '#C70039', '#900C3F', '#581845', '#0074D9', '#2ECC40', '#FFDC00'];
+
+setInterval(function()
+{
+
+    var rn_color = colors[Math.floor(Math.random() * colors.length)]
+    background.style.backgroundColor = rn_color;
+
+})
