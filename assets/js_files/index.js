ab2c72b3779eef870bbb2dc67c1c3f39b100a555
AverageDude <joshandre071605@gmail.com>
2023-03-11 20:45:47 +0800

    major changes

--- assets/js_files/index.js
+++ assets/js_files/index.js
@@ -10,10 +10,3 @@ setInterval(function()
 })
 
 
-// responsive top navigation bar
-const hamburger = document.getElementById("hamburger");
-const navLinks = document.getElementById("navLinks");
-
-hamburger.addEventListener("click", () => {
-	navLinks.classList.toggle("show");
-});
