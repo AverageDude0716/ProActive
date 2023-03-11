6d843febcfabbdf33719f18a6614bd5357760a10
Glitch (proactive) <none>
2023-03-11 03:01:42 +0000

    🎾🫀 Updated with Glitch

--- assets/js_files/index.js
+++ assets/js_files/index.js
@@ -1,6 +1,6 @@
+//for background
 var background = document.getElementById("background");
 var colors = ['#FF5733', '#C70039', '#900C3F', '#581845', '#0074D9', '#2ECC40', '#FFDC00'];
-
 setInterval(function()
 {
 
@@ -9,6 +9,8 @@ setInterval(function()
 
 })
 
+
+// responsive top navigation bar
 const menu_icon = document.querySelector(".menu_icon");
 const menu_bar = document.querySelector(".menu_bar");
 
---
+++ assets/php_files/sign_up_page.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Sign Up</title>
+    <link rel="icon" href="assets/media_files/evee logo.png">
+
+    <link rel="stylesheet" href="../css_files/sign_up_page.css">
+</head>
+<body>
+    
+
+
+</body>
+</html>
